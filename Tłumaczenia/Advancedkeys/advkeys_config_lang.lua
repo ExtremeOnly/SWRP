aca5c9b17246369c8efa6d34ad5f6a24f61ab67f
Zabójca997 <31369842+Zabujca997@users.noreply.github.com>
2021-10-23 02:46:48 +0200

    EQ Jobów
    Nareszcie EQ jobów

---
+++ Tłumaczenia/Advancedkeys/advkeys_config_lang.lua
@@ -0,0 +1,262 @@
+-- I DO NOT ADVISE YOU TRY TO EDIT BELOW THIS LINE UNLESS YOU UNDERSTAND HOW THIS WORKS!
+-- IF YOU WANT TO ADD A LANAGUE PLEASE OPEN A SUPPORT TICKET ON GMODSTORE.
+-- I WILL HELP YOU WITH ANYTHING YOU NEED AND ADD THE LANGUAGE TO THE OFFICIAL SCRIPT.
+CH_ADVKeys.Config.Lang = CH_ADVKeys.Config.Lang or {}
+
+CH_ADVKeys.Config.Lang["Your door has automatically locked."] = {
+	["en"] = "Your door has automatically locked.",
+	["fr"] = "Votre porte s'est verrouillée automatiquement.",
+	["da"] = "Din bildør har lukket automatisk.",
+	["de"] = "Deine Tür wurde automatisch verschlossen.",
+	["pl"] = "",
+	["ru"] = "Ваша дверь была автоматически закрыта.",
+	["es"] = "Tu puerta se ha cerrado automáticamente.",
+	["pt"] = "",
+	["cn"] = "",
+	["tr"] = "",
+	["no"] = "",
+	["pl"] = "Twoje drzwi zostały automatycznie zablokowane.",
+}
+
+CH_ADVKeys.Config.Lang["Somebody is stealing your vehicle!"] = {
+	["en"] = "Somebody is stealing your vehicle!",
+	["fr"] = "Quelqu'un vole votre véhicule!",
+	["da"] = "Nogen er i gang med at stjæle din bil!",
+	["de"] = "Jemand stiehlt dein Fahrzeug!",
+	["pl"] = "",
+	["ru"] = "Кто-то угоняет ваш автомобиль!",
+	["es"] = "¡Alguien está robando tu vehículo!",
+	["pt"] = "",
+	["cn"] = "",
+	["tr"] = "",
+	["no"] = "",
+	["pl"] = "Ktoś kradnie twój pojazd!",
+}
+
+CH_ADVKeys.Config.Lang["Your vehicle alarm has triggered. Your car is taking extreme damage!"] = {
+	["en"] = "Your vehicle alarm has triggered. Your car is taking extreme damage!",
+	["fr"] = "L'alarme de votre véhicule s'est déclenchée. Votre voiture subit des dommages extrêmes!",
+	["da"] = "Din bil tager skade og det har fået din bil alarm til at starte!",
+	["de"] = "Dein Fahrzeugalarm wurde ausgelöst. Dein Fahrzeug nimmt extremen Schaden!",
+	["pl"] = "",
+	["ru"] = "Сработала сигнализация вашего автомобиля. Ваша машина получает сильный урон!",
+	["es"] = "La alarma de tu vehículo se ha activado ¡Tu auto está a punto de romperse!",
+	["pt"] = "",
+	["cn"] = "",
+	["tr"] = "",
+	["no"] = "",
+	["pl"] = "Uruchomił się alarm Twojego pojazdu. Twój samochód ulega ekstremalnym uszkodzeniom!",
+}
+
+CH_ADVKeys.Config.Lang["You have successfully installed a vehicle alarm for your vehicle."] = {
+	["en"] = "You have successfully installed a vehicle alarm for your vehicle.",
+	["fr"] = "Vous avez installé avec succès une alarme pour votre véhicule.",
+	["da"] = "Du har installeret en bil alarm i din bil.",
+	["de"] = "Du hast erfolgreich eine Fahrzeugalarmanlage für dein Fahrzeug installiert.",
+	["pl"] = "",
+	["ru"] = "Вы успешно установили автомобильную сигнализацию для своего автомобиля.",
+	["es"] = "Has instalado correctamente una alarma en tu vehículo.",
+	["pt"] = "",
+	["cn"] = "",
+	["tr"] = "",
+	["no"] = "",
+	["pl"] = "Pomyślnie zainstalowałeś alarm pojazdu w swoim pojeździe.",
+}
+
+CH_ADVKeys.Config.Lang["The vehicle already has an alarm installed."] = {
+	["en"] = "The vehicle already has an alarm installed.",
+	["fr"] = "Le véhicule a déjà une alarme installée.",
+	["da"] = "Denne bil har allerede en alarm installeret.",
+	["de"] = "In dem Fahrzeug wurde bereits eine Alarmanlage installiert.",
+	["pl"] = "",
+	["ru"] = "Автомобиль уже имеет ​​сигнализацию.",
+	["es"] = "Tu vehículo ya tiene una alarma instalada.",
+	["pt"] = "",
+	["cn"] = "",
+	["tr"] = "",
+	["no"] = "",
+	["pl"] = "Pojazd ma już zainstalowany alarm.",
+}
+
+-- Keys weapon
+CH_ADVKeys.Config.Lang["Advanced Keys"] = {
+	["en"] = "Advanced Keys",
+	["fr"] = "Clés avancées",
+	["da"] = "Nøgler",
+	["de"] = "Erweiterte Schlüssel",
+	["pl"] = "",
+	["ru"] = "Расширенные Ключи",
+	["es"] = "Llaves Modernas",
+	["pt"] = "",
+	["cn"] = "",
+	["tr"] = "",
+	["no"] = "",
+	["pl"] = "Klucze zaawansowane",
+}
+
+CH_ADVKeys.Config.Lang["Left click to lock\nRight click to unlock\nReload for door settings or animation menu"] = {
+	["en"] = "Left click to lock\nRight click to unlock\nReload for door settings or animation menu",
+	["fr"] = "Clic gauche pour verrouiller \nClic droit pour déverrouiller \nRecharger pour les paramètres de porte ou le menu d'animation",
+	["da"] = "Venstre klik for at låse\nHøjre klik for at åbne\nReload for at åbne dør instillinger eller animations menuen.",
+	["de"] = "Linksklick zum Verriegeln\nRechtsklick zum Entriegeln\nReload (R) für Türeinstellungen oder dem Animationsmenü",
+	["pl"] = "",
+	["ru"] = "ЛКМ - запереть\nПКМ - открыть\nПерезарядка - настройки двери или меню анимаций",
+	["es"] = "Click izquierdo para cerrar\nClick derecho para abrir\nRecargar (R) para el menú de puerta o animaciones",
+	["pt"] = "",
+	["cn"] = "",
+	["tr"] = "",
+	["no"] = "",
+	["pl"] = "Kliknij lewym przyciskiem, aby zablokować\nKliknij prawym przyciskiem myszy, aby odblokować\nR, aby wyświetlić ustawienia drzwi lub menu animacji",
+}
+
+CH_ADVKeys.Config.Lang["You have locked the door on your vehicle."] = {
+	["en"] = "You have locked the door on your vehicle.",
+	["fr"] = "Vous avez verrouillé la porte de votre véhicule.",
+	["da"] = "Du har låst døren til din bil.",
+	["de"] = "Du hast die Tür deines Fahrzeugs verriegelt.",
+	["pl"] = "",
+	["ru"] = "Вы заперли дверь своего автомобиля.",
+	["es"] = "Has cerrado la puerta de tu vehículo.",
+	["pt"] = "",
+	["cn"] = "",
+	["tr"] = "",
+	["no"] = "",
+	["pl"] = "Zamknąłeś drzwi w swoim pojeździe.",
+}
+
+CH_ADVKeys.Config.Lang["You have locked the door."] = {
+	["en"] = "You have locked the door.",
+	["fr"] = "Vous avez verrouillé la porte.",
+	["da"] = "Du har låst døren.",
+	["de"] = "Du hast die Tür verriegelt.",
+	["pl"] = "",
+	["ru"] = "Вы заперли дверь.",
+	["es"] = "Has cerrado la puerta.",
+	["pt"] = "",
+	["cn"] = "",
+	["tr"] = "",
+	["no"] = "",
+	["pl"] = "Zamknąłeś drzwi.",
+}
+
+CH_ADVKeys.Config.Lang["You do not own this vehicle!"] = {
+	["en"] = "You do not own this vehicle!",
+	["fr"] = "Vous n'êtes pas propriétaire de ce véhicule!",
+	["da"] = "Du ejer ikke denne bil!",
+	["de"] = "Du bist nicht Eigentümer dieses Fahrzeugs!",
+	["pl"] = "",
+	["ru"] = "Вы не являетесь владельцем этого автомобиля!",
+	["es"] = "¡No eres dueño de este vehículo!",
+	["pt"] = "",
+	["cn"] = "",
+	["tr"] = "",
+	["no"] = "",
+	["pl"] = "Nie jesteś właścicielem tego pojazdu!",
+}
+
+CH_ADVKeys.Config.Lang["You have unlocked the door on your vehicle."] = {
+	["en"] = "You have unlocked the door on your vehicle.",
+	["fr"] = "Vous avez déverrouillé la porte de votre véhicule.",
+	["da"] = "Du har åbnet døren til din bil.",
+	["de"] = "Du hast die Tür an deinem Fahrzeug entriegelt.",
+	["pl"] = "",
+	["ru"] = "Вы открыли дверь своего автомобиля.",
+	["es"] = "Has abierto la puerta de tu vehículo.",
+	["pt"] = "",
+	["cn"] = "",
+	["tr"] = "",
+	["no"] = "",
+	["pl"] = "Otworzyłeś drzwi w swoim pojeździe.",
+}
+
+CH_ADVKeys.Config.Lang["You have unlocked the door."] = {
+	["en"] = "You have unlocked the door.",
+	["fr"] = "Vous avez déverrouillé la porte.",
+	["da"] = "Du har åbnet døren.",
+	["de"] = "Du hast die Tür entrickelt.",
+	["pl"] = "",
+	["ru"] = "Вы открыли дверь.",
+	["es"] = "Has abierto la puerta.",
+	["pt"] = "",
+	["cn"] = "",
+	["tr"] = "",
+	["no"] = "",
+	["pl"] = "Otworzyłeś drzwi.",
+}
+
+CH_ADVKeys.Config.Lang["You have successfully installed a door alarm for this door."] = {
+	["en"] = "You have successfully installed a door alarm for this door.",
+	["fr"] = "Vous avez installé avec succès une alarme de porte pour cette porte.",
+	["da"] = "Du har installeret en døralarm til denne dør.",
+	["de"] = "Sie haben erfolgreich einen Türalarm für diese Tür installiert.",
+	["pl"] = "",
+	["ru"] = "Вы успешно установили дверную сигнализацию для этой двери.",
+	["es"] = "Ha instalado correctamente una alarma para esta puerta.",
+	["pt"] = "",
+	["cn"] = "",
+	["tr"] = "",
+	["no"] = "",
+	["pl"] = "Pomyślnie zainstalowałeś alarm dla tych drzwi.",
+}
+
+CH_ADVKeys.Config.Lang["The door already has an alarm installed."] = {
+	["en"] = "The door already has an alarm installed.",
+	["fr"] = "La porte a déjà une alarme installée.",
+	["da"] = "Døren har allerede en alarm installeret.",
+	["de"] = "In der Tür ist bereits ein Alarm installiert.",
+	["pl"] = "",
+	["ru"] = "На двери уже установлена ​​сигнализация.",
+	["es"] = "La puerta ya tiene una alarma instalada.",
+	["pt"] = "",
+	["cn"] = "",
+	["tr"] = "",
+	["no"] = "",
+	["pl"] = "Drzwi mają już zainstalowany alarm.",
+}
+
+CH_ADVKeys.Config.Lang["Somebody is lockpicking a door of yours. It has triggered the door alarm!"] = {
+	["en"] = "Somebody is lockpicking a door of yours. It has triggered the door alarm!",
+	["fr"] = "Quelqu'un crochète une de vos portes. Il a déclenché l'alarme de la porte!",
+	["da"] = "Nogen prøver at bryde en dør op hos dig. Det har startet døralarmen!",
+	["de"] = "Jemand öffnet eine Tür von Ihnen. Es hat den Türalarm ausgelöst!",
+	["pl"] = "",
+	["ru"] = "Кто-то взламывает вашу дверь. Сработала дверная тревога!",
+	["es"] = "Alguien está forzando la cerradura en una puerta tuya. ¡Se ha activado la alarma!",
+	["pt"] = "",
+	["cn"] = "",
+	["tr"] = "",
+	["no"] = "",
+	["pl"] = "Ktoś otwiera twoje drzwi. Wywołał alarm w drzwiach!",
+}
+
+CH_ADVKeys.Config.Lang["You cannot install vehicle alarms on this particular vehicle."] = {
+	["en"] = "You cannot install vehicle alarms on this particular vehicle.",
+	["fr"] = "Vous ne pouvez pas installer d'alarmes de véhicule sur ce véhicule en particulier.",
+	["da"] = "Du kan ikke installere bilalarmer på netop dette køretøj.",
+	["de"] = "Sie können keine Fahrzeugalarme an diesem bestimmten Fahrzeug installieren.",
+	["pl"] = "",
+	["ru"] = "Вы не можете установить автомобильную сигнализацию на этот конкретный автомобиль.",
+	["es"] = "No puede instalar una alarma en este vehículo en particular.",
+	["pt"] = "",
+	["cn"] = "",
+	["tr"] = "",
+	["no"] = "",
+	["pl"] = "Nie możesz zainstalować alarmów pojazdu w tym konkretnym pojeździe!.",
+}
+
+--[[
+CH_ADVKeys.Config.Lang[""] = {
+	["en"] = "",
+	["fr"] = "",
+	["da"] = "",
+	["de"] = "",
+	["pl"] = "",
+	["ru"] = "",
+	["es"] = "",
+	["pt"] = "",
+	["cn"] = "",
+	["tr"] = "",
+	["no"] = "",
+	["pl"] = "",
+}
+--]]
